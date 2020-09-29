0047fb2ed179952fc73290f926ce0ec5bb1250f5
abed jamous <jamous.hameed94@gmail.com>
2020-09-29 16:15:08 -0400

    complete  delete like

--- src/components/messageComponent/MessageComponent.js
+++ src/components/messageComponent/MessageComponent.js
@@ -6,6 +6,7 @@ class Message extends React.Component {
     super(props);
     this.state = {
       likeCount: this.props.likes.length,
+      likes: this.props.likes,
       deleted: false,
     };
     this.client = new MessageService();
@@ -21,14 +22,33 @@ class Message extends React.Component {
   handleLike = () => {
     const messageService = new MessageService();
     const username = messageService.getUsername();
-    if (this.props.likes.some((like) => like.username === username)) return;
+    if (this.state.likes.some((like) => like.username === username)) return;
 
     messageService.postLike(this.props.id).then((like) => {
       console.log(like);
       this.setState((latestState) => ({
         likeCount: latestState.likeCount + 1,
+        likes: [...latestState.likes, like],
       }));
+      console.log(this.state.likes);
     });
+  };
+
+  removeLike = () => {
+    const messageService = new MessageService();
+    const username = messageService.getUsername();
+    const likeIndex = this.state.likes.findIndex(
+      (like) => (like.username = username)
+    );
+    if (this.state.likes.some((like) => like.username === username)) {
+      messageService.deleteLike(this.state.likes[likeIndex].id).then((like) => {
+        console.log(like);
+        this.setState((latestState) => ({
+          likeCount: latestState.likeCount - 1,
+          likes: [],
+        }));
+      });
+    }
   };
 
   render() {
@@ -47,6 +67,7 @@ class Message extends React.Component {
         {this.props.text}
         <div className="like-count">Likes: {this.state.likeCount}</div>
         <button onClick={this.handleLike}>Like</button>
+        <button onClick={this.removeLike}>Unlike</button>
         {Delete}
       </li>
     );
